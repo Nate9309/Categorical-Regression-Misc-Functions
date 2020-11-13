53c0144c6f870647b5783a3afc80271315a5843f
Nathan Duma <11836974+Nate9309@users.noreply.github.com>
2020-11-12 19:38:32 -0500

    Update Misc_Functions.R
    Add hmTestR function for IIA testing mlogit

--- Misc_Functions.R
+++ Misc_Functions.R
@@ -45,12 +45,14 @@ SDofX = standard deviation of X\n\n\n"
 
 # Lecture 4 (Multinomial Models) ------
 
+## Wald and Likelihood ratio tests for mlogit objects -----
+
 mlogTestR <- function(unrestrictedModel, reflevel, type = "wald", digits = 4) {
   # Inputs: unrestrictedmodel is a mlogit object
   #         reflevel is the reference level of the dependent variable
   #         type default is the wald test, It can be changed to "lr" i.e. likelihood ratio
   #         Rounded to 4 decimal places by default.
-  # Output: Datafram containing the results of the wald/lr test similar to Stata's mlogtest, wald lr
+  # Output: Dataframe containing the results of the wald/lr test similar to Stata's mlogtest, wald lr
   depVarList <- gsub(" \\+", ",", terms(unrestrictedModel)[[3]][3]) # get dep vars
   depVarList <- unlist(strsplit(depVarList, ", "))
   
@@ -87,4 +89,44 @@ mlogTestR <- function(unrestrictedModel, reflevel, type = "wald", digits = 4) {
   
   # Output
   return(resultDF1)
+}
+
+
+
+## Hausman-McFadden tests for mlogit models ----
+
+hmTestR <- function(unrestrictedModel, reflevel, digits = 4){
+  # Performs a Hausman-McFadden to test the assumption of IIA for multinomial logit models
+  # Inputs: unrestrictedmodel is a mlogit object
+  #         reflevel is the reference level of the dependent variable in the unrestricted model
+  #         Rounded to 4 decimal places by default.
+  # Output: Dataframe containing the results of the Hausman-MacFadden test similar to Stata's mlogtest, hau
+
+  categoryList <- names(unrestrictedModel$freq) # list of categories
+  categoryList <- categoryList[categoryList != reflevel] # drop base level
+  
+  unrestrictedFormula <- formula(unrestrictedModel)
+  
+  resultDF <- data.frame(Omitted = character(), chi2 = numeric(), df = numeric(),
+                         P.chi2 = character())
+  
+  for(j in 1:length(categoryList)){
+    tempReg <- mlogit(unrestrictedFormula, data = unrestrictedModel$model, reflevel = reflevel,
+                      alt.subset = c(categoryList[-j], reflevel))
+    hmResult <- hmftest(unrestrictedModel, z = tempReg) # do hausman test
+    hmResult <- unlist(hmResult)
+    
+    resultDF <- rbind(resultDF, data.frame(Omitted = categoryList[j], chi2 = round(hmResult$statistic.chisq, digits = digits), df = hmResult$parameter.df,
+                                           P.chi2 = ifelse(hmResult$statistic.chisq > 0, round(hmResult$p.value.chisq, digits = digits), "---")))
+  }
+  cat(paste("Hausman-McFadden tests of IIA assumption (N=", length(unrestrictedModel$fitted.values),")\n",sep = ""))
+  cat("Ho: Odds(Outcome-J vs Outcome-K) are independent of other alternatives.\n\n")
+  
+  cat("Note: If chi2<0, the estimated model does not meet the asymptotic assumptions of the test.\n\n")
+  
+  resultDF <- as.data.frame(resultDF, row.names = NULL)
+  
+  
+  # Output
+  return(resultDF)
 }
